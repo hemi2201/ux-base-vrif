3b5d1fceb372a5626a111c3ac040c58257a251ec
henrymik <113172684+hemi2201@users.noreply.github.com>
2023-03-24 08:46:33 +0100

    Set pieces to snap grab

---
+++ Assets/_The Seventh Seal VR_/Scripts/ResetChessPieces.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetChessPieces : MonoBehaviour
+{
+    [SerializeField] private Transform[] chessPieces;
+}
