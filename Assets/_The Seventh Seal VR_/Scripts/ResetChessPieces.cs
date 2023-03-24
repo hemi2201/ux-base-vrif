b7f49770644b0e33d30815bce51f0d65c03fbaa2
henrymik <113172684+hemi2201@users.noreply.github.com>
2023-03-24 12:51:01 +0100

    audio setup

--- Assets/_The Seventh Seal VR_/Scripts/ResetChessPieces.cs
+++ Assets/_The Seventh Seal VR_/Scripts/ResetChessPieces.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class ResetChessPieces : MonoBehaviour
 {
     [SerializeField] private Transform[] chessPieces;
+    [SerializeField] private Transform[] spawnPos;
+
+    private int numOfPieces = 32;
+
+    public void resetGame()
+    {
+        for (int i = 0; i < numOfPieces; i++)
+        {
+            chessPieces[i].position = spawnPos[i].position;
+            chessPieces[i].rotation = spawnPos[i].localRotation;
+        }
+    }
 }
