4df417e05d8e3e35067c02005432370aac41a5a9
henrymik <113172684+hemi2201@users.noreply.github.com>
2023-03-26 12:55:56 +0200

    Spawn to new map plus buggy spawn positions

--- Assets/_The Seventh Seal VR_/Scripts/SpawnManager.cs
+++ Assets/_The Seventh Seal VR_/Scripts/SpawnManager.cs
@@ -8,7 +8,10 @@ public class SpawnManager : MonoBehaviour
 {
     //[SerializeField] GameObject GenericVRPlayerPrefab;
 
-    //public Vector3 spawnPosition;
+    public Transform[] startPos;
+    public Transform XRRig;
+    public Vector3 spawnPosition;
+    public Vector3 spawnRotation;
 
     [Tooltip("Name of the Player object to spawn. Must be in a /Resources folder.")]
     public string RemotePlayerObjectName = "RemotePlayer";
@@ -16,12 +19,28 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            XRRig.position = startPos[0].position;
+            spawnPosition = startPos[0].position;
+            spawnRotation = new Vector3(0, 0, 0);
+        }
+        else if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            XRRig.position = startPos[1].position;
+            XRRig.localRotation = startPos[1].localRotation;
+            spawnPosition = startPos[1].position;
+
+            
+
+        }
+
         if (PhotonNetwork.IsConnectedAndReady)
         {
             //PhotonNetwork.Instantiate(GenericVRPlayerPrefab.name, spawnPosition, Quaternion.Euler(0.0f, 90.0f, 0f));
 
             // Network Instantiate the object used to represent our player. This will have a View on it and represent the player         
-            GameObject player = PhotonNetwork.Instantiate(RemotePlayerObjectName, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
+            GameObject player = PhotonNetwork.Instantiate(RemotePlayerObjectName, spawnPosition, Quaternion.identity, 0);
             BNG.NetworkPlayer np = player.GetComponent<BNG.NetworkPlayer>();
             if (np)
             {
