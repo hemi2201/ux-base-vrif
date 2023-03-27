89a7e05bf54e6028cc613a359b10fc601d1f16a8
henrymik <113172684+hemi2201@users.noreply.github.com>
2023-03-27 10:09:38 +0200

    UI updates

---
+++ Assets/_The Seventh Seal VR_/Scripts/ToggleUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace BNG {
+
+    /// <summary>
+    /// This script will toggle a GameObject whenever the provided InputAction is executed
+    /// </summary>
+    public class ToggleUI : MonoBehaviour {
+
+        public InputActionReference InputAction = default;
+        public GameObject ToggleObject = default;
+        Canvas uICanvas;
+        bool canvasEnabled = false;
+
+        private void Start()
+        {
+                uICanvas = ToggleObject.GetComponent<Canvas>();
+                uICanvas.enabled = canvasEnabled;
+        }
+
+        private void OnEnable() {
+            InputAction.action.performed += ToggleActive;
+        }
+
+        private void OnDisable() {
+            InputAction.action.performed -= ToggleActive;
+        }
+
+        public void ToggleActive(InputAction.CallbackContext context) {
+            if(ToggleObject) {
+                uICanvas.enabled = !canvasEnabled;
+                canvasEnabled = !canvasEnabled;
+            }
+        }
+    }
+}
+
---
+++ Assets/_The Seventh Seal VR_/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class UIManager : MonoBehaviour
+{
+    public UnityEvent UIMenuToggle;
+
+
+    //public void ToggleUIMenu()
+    //{
+    //    UIMenu.s
+    //}
+}
